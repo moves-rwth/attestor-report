9eedbc0d79f828821e0dc05635964206862589fe
Christina Jansen <28920918+ChrJansen@users.noreply.github.com>
2017-11-29 13:39:55 +0100

    Dashboard input.

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { Http, Response } from '@angular/http';
+
+import "rxjs/add/operator/takeWhile";
+import 'rxjs/add/operator/catch';
+
 
 @Component({
     selector: 'app-dashboard',
@@ -8,34 +13,77 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class DashboardComponent implements OnInit {
-    public alerts: Array<any> = [];
-    
-    constructor() {
+    alive : boolean;
 
-        this.alerts.push({
-            id: 1,
-            type: 'success',
-            message: `No null pointer dereferences detected.`
-        }, {
-            id: 2,
-            type: 'warning',
-            message: `Null pointer dereferences detected.`
-        }, {
-            id: 2,
-            type: 'success',
-            message: `State space generation successful.`
-        }, {
-          id: 3,
-          type: 'warning',
-          message: 'State space generation aborted (maximal state space or heap size exceeded).'
-        });
+    public alerts: Array<any> = [];
+    public messages: Array<any> = [];
+    public numberStates : number;
+    public numberTermStates : number;
+    public numberSuccessfulForm : number;
+    public numberFailForm : number;
+
+    constructor(private http:Http) {
+      this.alive = true;
+
+      // Load json file with general attestor output
+      console.log("Read summary file");
+      this.readGeneralInfoJSONFile().subscribe(result => {
+                                              console.log(result);
+                                              this.numberStates = result["0"].summary["0"].numberStates;
+                                              this.numberTermStates = result["0"].summary["1"].numberTerminalStates;
+                                              this.numberSuccessfulForm = result["0"].summary["2"].numberFormulaeSuccess;
+                                              this.numberFailForm = result["0"].summary["3"].numberFormulaeFail;
+                                              this.messages = result["3"].messages;
+
+                                              this.fillAlerts(this.messages);
+                                          });
     }
 
     ngOnInit() {
     }
 
+    public fillAlerts(input:Array<any>){
+      let type : string = "";
+      let message : any;
+      for(message in input){
+        console.log(message);
+        console.log("Message type: " + input[message].type);
+        switch(input[message].type){
+          case "error": type = "danger"; break;
+          case "info": type = "info"; break;
+          case "warn": type = "warning"; break;
+          case "success": type = "success"; break;
+        }
+
+        this.alerts.push({
+          id: message,
+          type: type,
+          message: input[message].message
+        });
+      }
+    }
+
     public closeAlert(alert: any) {
         const index: number = this.alerts.indexOf(alert);
         this.alerts.splice(index, 1);
+    }
+
+    ngOnDestroy(){
+      this.alive = false;
+    }
+
+    readGeneralInfoJSONFile(){
+      // get users from api
+          return this.http.get('assets/attestorOutput/testSummary.json')//, options)
+              .takeWhile(() => this.alive)
+              .map((response: Response) => {
+                  return response.json();
+              }
+          )
+          .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
     }
 }
