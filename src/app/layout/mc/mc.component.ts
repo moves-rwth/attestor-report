b27a34cd408a066ac70e6042cd37afcae65c600d
Christina Jansen <28920918+ChrJansen@users.noreply.github.com>
2017-12-21 14:25:07 +0100

    Input visualisation: formulae.

--- src/app/layout/mc/mc.component.ts
+++ src/app/layout/mc/mc.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import {Http, Response, RequestOptions, Headers} from '@angular/http';
+import { Subscription } from "rxjs/Subscription";
+import 'rxjs/add/operator/catch';
+import * as cytoscape from 'cytoscape';
+import 'rxjs/add/operator/toPromise';
+import "rxjs/add/operator/takeWhile";
 
 @Component({
     selector: 'app-mc',
@@ -9,10 +15,45 @@ import { routerTransition } from '../../router.animations';
 })
 export class MCComponent implements OnInit {
 
-    constructor() {
+    alive : boolean;
+
+    formulae : Array<string>;
+
+    constructor(private http:Http) {
+
+      this.alive = true;
+
+
+      // Get the input MC formulae from settings file
+      this.readSettingsJSONFile().subscribe(result => {
+
+        this.formulae = result.modelChecking.formulae.split(';');
+        console.log(this.formulae);
+
+      });
 
     }
 
     ngOnInit() {
     }
+
+    readSettingsJSONFile(){
+      // get the summary information of the initial HCs
+          return this.http.get('assets/attestorInput/settings.json')//, options)
+              .takeWhile(() => this.alive)
+              .map((response: Response) => {
+                  return response.json();
+              }
+          )
+          .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+    }
+
+    ngOnDestroy() {
+      this.alive = false;
+    }
+
 }
