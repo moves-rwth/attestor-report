d7c7324feb466f0f1eeadb1b6637d7e39c9128d7
Christina Jansen <28920918+ChrJansen@users.noreply.github.com>
2018-01-09 09:05:19 +0100

    Initial output of Attestor options.

--- src/app/layout/options/options.component.ts
+++ src/app/layout/options/options.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import {Http, Response, RequestOptions, Headers} from '@angular/http';
+import { Subscription } from "rxjs/Subscription";
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+import "rxjs/add/operator/takeWhile";
 
 @Component({
     selector: 'app-options',
@@ -9,10 +14,61 @@ import { routerTransition } from '../../router.animations';
 })
 export class OptionsComponent implements OnInit {
 
-    constructor() {
+    alive : boolean;
+
+    booleanOptions : Array<any> = new Array();
+
+    otherOptions : Array<any> = new Array();
+
+    constructor(private http:Http) {
+      this.alive = true;
+
+      this.readOptionsJSONFile().subscribe(result => {
+        //this.options = result;
+
+        // Partition into boolean and other option values
+        for(let option of result){
+          switch(option.value){
+            case 'true':{
+              this.booleanOptions.push(option);
+              break;
+            }
+            case 'false':{
+              this.booleanOptions.push(option);
+              break;
+            }
+
+            default: {
+              this.otherOptions.push(option);
+              break;
+            }
+          }
+        }
+
+      });
 
     }
 
+    readOptionsJSONFile(){
+      // get all (displayed) options together with its values (even if default)
+          return this.http.get('assets/attestorInput/options.json')//, options)
+              .takeWhile(() => this.alive)
+              .map((response: Response) => {
+                  return response.json();
+              }
+          )
+          .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+    }
+
+
     ngOnInit() {
+    }
+
+    ngOnDestroy() {
+      this.alive = false;
     }
 }
